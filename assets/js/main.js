ff53d571fcd710b4ce2fd38c7cb9b7acf2144acc
abhishekchandra2522k <1ac23456789@gmail.com>
2021-03-17 22:06:20 +0530

    modify main.js, add scroll, header, nav functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,51 @@ const showMenu = (toggleId, navId) =>{
 
 showMenu('nav-toggle','nav-menu')
 
+// Remove menu mobile
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+// Scroll sections active link
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop &&  scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+// Change background header
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+// show scroll top
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('scroll-header'); else scrollTop.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
 /*----- ANIMATE -----*/
 // OVERLAY
 gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
