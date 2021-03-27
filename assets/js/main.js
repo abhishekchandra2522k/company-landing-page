ae9704d343b2e14a53896db2ea76eaa45da124b5
abhishekchandra2522k <1ac23456789@gmail.com>
2021-03-27 11:56:03 +0530

    Modify main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,53 +144,28 @@ function showDivs(n) {
     dots[slideIndex-1].className += " w3-opacity-off";
 }
 
-let slider = document.querySelector('.slider');
-let innerSlider = document.querySelector('.slider-inner');
-
-let pressed = false;
-let startx;
-let x;
-
-slider.addEventListener('mousedown', (e)=>{
-    pressed = true;
-    startx = e.offsetX - innerSlider.offsetLeft;
-    slider.style.cursor = 'grabbing'
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 700,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
+    })
 });
-
-// slider.addEventListener('mouseenter', ()=>{
-//     slider.style.cursor = 'grab'
-// });
-
-// slider.addEventListener('mouseleave',()=>{
-//     slider.style.cursor = 'default'
-// })
-
-slider.addEventListener('mouseup',()=>{
-    slider.style.cursor = 'grab'
-});
-
-window.addEventListener('mouseup', ()=>{
-    pressed = false;
-});
-
-slider.addEventListener('mousemove',(e)=>{
-    if(!pressed) return;
-    e.preventDefault();
-
-    x = e.offsetX
-    
-    innerSlider.style.left = `${x - startx}px`;
-
-    checkboundary()
-});
-
-function checkboundary(){
-    let outer = slider.getBoundingClientRect();
-    let inner = innerSlider.getBoundingClientRect();
-
-    if(parseInt(innerSlider.style.left) > 0){
-        innerSlider.style.left = '0px';
-    }else if(inner.right < outer.right){
-        innerSlider.style.left = `-${inner.width - outer.width}px`
-    }
-}
