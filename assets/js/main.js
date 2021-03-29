d6ba929bf3659f4ca18441658995ab5fee020fc8
abhishekchandra2522k <1ac23456789@gmail.com>
2021-03-29 21:22:51 +0530

    Modify main.js, update scroll reveal, add read more in services section

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,9 +92,9 @@ themeButton.addEventListener('click', () => {
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '30px',
+    distance: '50px',
     duration: 1000,
-    reset: true
+    reset: false
 });
 
 sr.reveal(`.home__data, .home__img,
@@ -111,6 +111,38 @@ function myFunction() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  
+function myFunction1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  
+function myFunction2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
   
     if (dots.style.display === "none") {
       dots.style.display = "inline";
